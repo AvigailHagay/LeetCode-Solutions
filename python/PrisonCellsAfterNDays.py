a3e305a8a6d860e63104d16efd45712f2bd5ff5f
Avigail Hagay <106158965+AvigailHagay@users.noreply.github.com>
2023-08-22 15:10:56 +0300

    Create PrisonCellsAfterNDays.py

---
+++ python/PrisonCellsAfterNDays.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def prisonAfterNDays(self, cells, n):
+        """
+        :type cells: List[int]
+        :type n: int
+        :rtype: List[int]
+        """
+        seen = {str(cells): n}
+        while n:
+            seen.setdefault(str(cells),n)
+            n -= 1
+            cells = [0] + [cells[i - 1] ^ cells[i + 1] ^ 1 for i in range(1, 7)] + [0]
+            if str(cells) in seen:
+                n %= seen[str(cells)] - n
+        return cells
